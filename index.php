c600d0cda4d44ef9310f2fe0c58f1014c51a447d
Kovchin <kovchin@mail.ru>
2020-05-22 12:57:15 +0700

    22.05.2020

--- index.php
+++ index.php
@@ -11,6 +11,29 @@
 
     <a href='./pages/summ.php'>Итоги</a>
 
+    <!--Форма для загрузки файлов на сервер-->
+
+    <form action="/core/upload.php" method="post" enctype="multipart/form-data">
+
+        <input type="file" name="file">
+
+        <input type="submit" value="Отправить">
+
+    </form>
+
+    <!--
+    Множественная загрузка файлов
+        
+<form action="/upload.php" method="post" enctype="multipart/form-data">
+
+	<input type="file" name="file[]" multiple>
+
+	<input type="submit" value="Отправить">
+
+</form>
+
+    -->
+
 </body>
 
 </html>
